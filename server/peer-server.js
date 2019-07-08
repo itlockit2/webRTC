24a2460ba8d39a7f564a2e5ae253492eb4673217
SUNGHO YUN <itlockit@gmail.com>
2019-07-09 01:12:56 +0900

    deploy

---
+++ server/peer-server.js
@@ -0,0 +1,12 @@
+// project/server/peer-server.js
+var fs = require("fs");
+var PeerServer = require("peer").PeerServer;
+
+var server = PeerServer({
+  port: 9000,
+  path: "/peerjs",
+  ssl: {
+    key: fs.readFileSync("./../certificates/key.pem", "utf8"),
+    cert: fs.readFileSync("./../certificates/cert.pem", "utf8")
+  }
+});
